18d42ae380142a0c056991cbb307963b6da52aef
T.M <gychaobest@foxmail.com>
2020-01-14 20:31:29 +0800

    add test simple

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 
 var Bridge = require("./bridge/bridge")
 var App = require("./app/lifecycle")
+var Auth = require("./open/authorize/index")
 
 var nchsdk = {
         version: '1.0.0',
         bridge: Bridge,
         app: App,
+        auth: Auth,
 }
 
 // dont override global variable
