32986cdb4b21186825502ca247bec3eb40f275df
Bao Jun <baojun.cz@gmail.com>
2020-01-15 19:27:26 +0800

    feat: add status code

--- src/open/authorize/index.js
+++ src/open/authorize/index.js
@@ -3,6 +3,45 @@ var Bridge = require('../../bridge/bridge')
 
 var auth = {
 
+        authStatus: {
+                0: "OK",
+                1: "unknow error",
+                2: "invalid signature",
+                3: "invalid timestamp",
+                4: "invalid publicKey"
+        },
+
+        /** 
+         *  publicKey 应用公钥
+         *  timestamp 时间戳（客服端将校验，一分钟内时间戳有效）
+         *  signature 对 timestamp 进行 Sha3 后签名的十六进制结果
+         *  建议 signature 使用服务端签名（ecdsa） ，请勿将私钥写在网页中！！
+         *  result: { status : 0 , result : "" }
+        **/
+        login: function (icon, name, publicKey, timestamp, signature) {
+                console.log('login')
+                return new Promise(function (resolve, reject) {
+                        var callback = function (res) {
+                                if (res.status = 0) {
+                                        resolve(res)
+                                } else {
+                                        reject(res)
+                                }
+                        }
+                        // android
+                        if (window.nchPlugin) {
+                                var callname = Bridge._dealCallback(callback)
+                                window.nchPlugin.authLogin(icon, name, publicKey, timestamp, signature, callname)
+                        }
+                        else {
+                                var params = {
+                                        icon, name, originText, signature
+                                }
+                                Bridge.sendRequest('authLogin', params, callback)
+                        }
+                });
+        },
+
         testInterface: function (params) {
                 console.log("test 2")
                 return new Promise(function (resolve, reject) {
@@ -21,33 +60,6 @@ var auth = {
                         }
                 });
         },
-
-        /** publicKey 应用公钥
-         *  timestamp 时间戳（客服端将校验，一分钟内时间戳有效）
-         *  signature 对publicKey+timestamp 签名
-         *  建议 signature 使用服务端签名（ecdsa） ，请勿将私钥写在网页中！！
-        **/
-        login: function (icon, name, publicKey, timestamp, signature) {
-                console.log('login')
-                return new Promise(function (resolve, reject) {
-                        var callback = function (res) {
-                                resolve(res)
-                        }
-                        // android
-                        if (window.nchPlugin) {
-                                var callname = Bridge._dealCallback(callback)
-                                window.nchPlugin.authLogin(icon, name, publicKey, timestamp, signature, callname)
-                        }
-                        else {
-                                var params = {
-                                        icon, name, originText, signature
-                                }
-                                Bridge.sendRequest('authLogin', params, callback)
-                        }
-                });
-        }
-
-
 }
 
 module.exports = auth
